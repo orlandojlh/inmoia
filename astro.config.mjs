74679491ee3cd394cc80e2c7fe1a6626a70f171d
orlandojlh <orlandojlh@gmail.com>
2026-06-08 16:31:36 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://inmoia.vercel.app',
+  site: 'https://inmoia-eosin.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
